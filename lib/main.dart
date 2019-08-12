57bb19a9fb41c551e8ce92b1de0d76efc77f3eff
Leandro Pinto <leandro.pnto@gmail.com>
2019-08-11 23:47:59 -0300

    Grid de imagens

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,12 @@ import 'package:loja_virtual/pages/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
+    title: 'CloudNine Clothing',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+      primaryColor: Color.fromARGB(255, 4, 125,141) 
+    ),
+    debugShowCheckedModeBanner: false,
     home: HomePage(),
   ));
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/tabs/home_tab.dart';
 
 class HomePage extends StatelessWidget {
+
+  final PageController pageController = PageController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Loja Virtual'),
-      ),
+    return PageView(
+      controller: pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        HomeTab()
+      ],
     );
   }
 }
