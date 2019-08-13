37d0e64d7df8ec7e5640452554a7195606b54bfd
lpinto <lpinto@crm-rj.gov.br>
2019-08-13 18:10:28 -0300

    Métodos de criação do usuário

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,14 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class UserModel extends Model{
-  
+class UserModel extends Model {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseUser firebaseUser;
+  Map<String, dynamic> userData = Map();
+
   bool isLoading = false;
 
-  void signUp(){
+  void signUp(
+      {@required Map<String, dynamic> userData,
+      @required String pass,
+      @required VoidCallback onSuccess,
+      @required VoidCallback onFail}) {
+    isLoading = true;
+    notifyListeners();
 
+    _auth
+        .createUserWithEmailAndPassword(
+            email: userData['email'], password: pass)
+        .then((user) async {
+      firebaseUser = user;
+      await _saveUserData(userData);
+      onSuccess();
+      isLoading = false;
+      notifyListeners();
+    }).catchError((e) {
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
   }
 
-  void signIn() async{
+  void signIn() async {
     isLoading = true;
     notifyListeners();
     await Future.delayed(Duration(seconds: 3));
@@ -17,11 +43,17 @@ class UserModel extends Model{
     notifyListeners();
   }
 
-  void recoverPass(){
+  void recoverPass() {}
 
+  bool isLoggedIn() {
+    return false;
   }
 
-  bool isLoggedIn(){
-    return false;
+  Future<Null> _saveUserData(Map<String, dynamic> userData) async {
+    this.userData = userData;
+    await Firestore.instance
+        .collection('users')
+        .document(firebaseUser.uid)
+        .setData(userData);
   }
 }
