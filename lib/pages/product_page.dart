923c801c8e33b934a7b620b41f4531f5872f390e
lpinto <lpinto@crm-rj.gov.br>
2019-08-13 11:33:09 -0300

    Implementando seleção do tamanho

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
 
 class ProductPage extends StatefulWidget {
-
   final ProductData product;
 
   const ProductPage({Key key, this.product}) : super(key: key);
@@ -13,10 +12,10 @@ class ProductPage extends StatefulWidget {
 }
 
 class _ProductPageState extends State<ProductPage> {
-
   final ProductData product;
 
   _ProductPageState(this.product);
+  String productSize;
 
   @override
   Widget build(BuildContext context) {
@@ -30,13 +29,11 @@ class _ProductPageState extends State<ProductPage> {
       body: ListView(
         children: <Widget>[
           AspectRatio(
-            aspectRatio: 0.9,
+            aspectRatio: 0.95,
             child: Carousel(
-              images: product.images.map( 
-                (url) {
-                  return NetworkImage(url);
-                }
-              ).toList(),
+              images: product.images.map((url) {
+                return NetworkImage(url);
+              }).toList(),
               dotSize: 4.0,
               dotSpacing: 15.0,
               dotBgColor: Colors.transparent,
@@ -51,10 +48,7 @@ class _ProductPageState extends State<ProductPage> {
               children: <Widget>[
                 Text(
                   product.title,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w500
-                  ),
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
                   maxLines: 3,
                 ),
                 Text(
@@ -63,7 +57,46 @@ class _ProductPageState extends State<ProductPage> {
                     fontSize: 22.0,
                     fontWeight: FontWeight.bold,
                     color: primaryColor,
-                    
+                  ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                  'Tamanho',
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 34.0,
+                  child: GridView(
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 1,
+                        mainAxisSpacing: 8.0,
+                        childAspectRatio: 0.5),
+                    children: product.sizes.map((size) {
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            productSize = size;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4.0)),
+                              border: Border.all(
+                                  color: size == productSize
+                                      ? primaryColor
+                                      : Colors.grey[500],
+                                  width: 3.0)),
+                          width: 50.0,
+                          alignment: Alignment.center,
+                          child: Text(size),
+                        ),
+                      );
+                    }).toList(),
                   ),
                 ),
               ],
