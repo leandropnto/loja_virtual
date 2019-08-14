99cca4b92b2a96b3e3fa4be176af1e4aec97f9cc
lpinto <lpinto@crm-rj.gov.br>
2019-08-14 11:29:36 -0300

    ProductData

---
+++ lib/datas/cart_product.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:loja_virtual/datas/product_data.dart';
+
+class CartProduct {
+  String cid; //cart id
+  String category;
+  String pid; //product id
+  int quantity;
+  String size;
+
+  ProductData productData;
+
+  CartProduct.fromDocument(DocumentSnapshot document) {
+    cid = document.documentID;
+    category = document.data['category'];
+    pid = document.data['pid'];
+    quantity = document.data['quantity'];
+    size = document.data['size'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "category": category,
+      "quantity": quantity,
+      "pid": pid,
+      "size": size,
+      "product": productData.toResumeMap()
+    };
+  }
+}
