5d0495ffab6cfd260407f49bfda9a27407358d33
Leandro Pinto <leandro.pnto@gmail.com>
2019-08-15 21:39:27 -0300

    Designer cardTitle

--- lib/tiles/cart_tile.dart
+++ lib/tiles/cart_tile.dart
@@ -10,6 +10,75 @@ class CartTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).primaryColor;
+
+    final priceStyle = TextStyle(
+        color: primaryColor, fontSize: 16, fontWeight: FontWeight.bold);
+
+    final TextStyle titleStyle =
+        TextStyle(fontWeight: FontWeight.w500, fontSize: 17);
+
+    final TextStyle sizeStyle = TextStyle(fontWeight: FontWeight.w300);
+
+    Widget _buildContent() {
+      return Row(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8),
+            width: 120,
+            child: Image.network(
+              cartProduct.productData.images[0],
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: Container(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      cartProduct.productData.title,
+                      style: titleStyle,
+                    ),
+                    Text("Tamanho: ${cartProduct.size}", style: sizeStyle),
+                    Text(
+                      "R\$ ${cartProduct.productData.price.toStringAsFixed(2)}",
+                      style: priceStyle,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(
+                            Icons.remove,
+                            color: primaryColor,
+                          ),
+                          onPressed: cartProduct.quantity > 1 ? () {} : null,
+                        ),
+                        Text(cartProduct.quantity.toString()),
+                        IconButton(
+                          icon: Icon(
+                            Icons.add,
+                            color: primaryColor,
+                          ),
+                          onPressed: () {},
+                        ),
+                        FlatButton(
+                          child: Text('Remover'),
+                          textColor: Colors.grey[500],
+                          onPressed: () {},
+                        )
+                      ],
+                    )
+                  ],
+                )),
+          ),
+        ],
+      );
+    }
+
     return Card(
       margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
       child: cartProduct.productData == null
@@ -33,15 +102,7 @@ class CartTile extends StatelessWidget {
                   .collection('items')
                   .document(cartProduct.pid)
                   .get())
-          : null,
-    );
-  }
-
-  Widget _buildContent() {
-    return Row(
-      children: <Widget>[
-        Image.network(cartProduct.productData.images[0], height: 70,),
-      ],
+          : _buildContent(),
     );
   }
 }
